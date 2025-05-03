b5de62824e4b4e3281eb316419239bef3d8a4d41
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-03 21:35:37 +0200

    Update Aereo.java
    scritto costruttore della classe aereo

--- Aereo.java
+++ Aereo.java
@@ -2,7 +2,7 @@ public class Aereo {
 
     //attributi
     private String ID;
-    private boolean soloAndata;
+    private boolean isSoloAndata;
     private int codiceVolo;
     private String impresaCostruttrice;
     private boolean deveAtterrare;
@@ -17,4 +17,23 @@ public class Aereo {
 
     private double maxLitriSerbatoioBenzina;
     private double livelloBenzina;
+
+    public Aereo(String ID, boolean isSoloAndata, int codiceVolo, String impresaCostruttrice, boolean deveAtterrare, int kgMax, int kgDaCaricare, int numPersoneDaCaricare, int numPersoneMassime, double maxLitriSerbatoioBenzina){
+        this.ID = ID;
+        this.isSoloAndata = isSoloAndata;
+        this.codiceVolo = codiceVolo;
+        this.impresaCostruttrice = impresaCostruttrice;
+        this.deveAtterrare = deveAtterrare;
+
+        this.kgMax = kgMax;
+        this.kgDaCaricare = kgDaCaricare;
+        this.kgEffetiviSuAereo = 0;
+
+        this.numPersoneDaCaricare = numPersoneDaCaricare;
+        this.numPersoneMassime = numPersoneMassime;
+        this.numPersoneEffetiveSuAereo = 0;
+
+        this.maxLitriSerbatoioBenzina = maxLitriSerbatoioBenzina;
+        this.livelloBenzina = 0;
+    }
 }
