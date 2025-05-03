85e0ab2edd80b20b05fbf6f80a88464d217e06fe
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-03 22:20:22 +0200

    Update VeicoloBagagli.java
    scritta funzione per caricare i bagagli sull'aereo

--- VeicoloBagagli.java
+++ VeicoloBagagli.java
@@ -7,4 +7,28 @@ public class VeicoloBagagli {
     public VeicoloBagagli(){
         this.semaforoVeicoloBagagli = new Semaforo(1);
     }
+
+
+
+
+
+
+
+
+
+
+    public void inserisciBagagli(Aereo aereo){
+        this.semaforoVeicoloBagagli.p();
+
+        if (aereo.getKgDaCaricare() > aereo.getKgMax()) {
+            System.out.println("\nCaricamento bagagli per "+ aereo.getID()+"; caricati per motivi di capienza solo: "+aereo.getKgMax()+" kg");
+            aereo.setKgEffetiviSuAereo(aereo.getKgMax());
+        }else{
+            System.out.println("\nCaricamento bagagli per "+ aereo.getID()+", tutti i bagali caricati corretamente, sono stati caricati: "+aereo.getKgDaCaricare()+" kg");
+            aereo.setKgEffetiviSuAereo(aereo.getKgDaCaricare());
+        }
+        
+       
+        this.semaforoVeicoloBagagli.v();
+    }
 }
