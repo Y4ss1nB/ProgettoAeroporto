ea263c05a8142e051931d1a2517be0ac60300e26
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-03 22:17:01 +0200

    Update VeicoloBagagli.java
    scritto semaforo che mette il semaforo ad 1 perché c'è un solo veicolo bagagli

--- VeicoloBagagli.java
+++ VeicoloBagagli.java
@@ -3,5 +3,8 @@ public class VeicoloBagagli {
     //attributo
     private Semaforo semaforoVeicoloBagagli;
 
-    
+    //cosstruttore
+    public VeicoloBagagli(){
+        this.semaforoVeicoloBagagli = new Semaforo(1);
+    }
 }
