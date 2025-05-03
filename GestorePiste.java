f986bea72a30b3f9bcfd52a10c5dfeca7688affc
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-03 22:07:40 +0200

    Update GestorePiste.java
    scritta funzione per far usare una pista ad un aereo

--- GestorePiste.java
+++ GestorePiste.java
@@ -7,4 +7,17 @@ public class GestorePiste {
     public GestorePiste(){
         this.semaforoPiste = new Semaforo(3);
     }
+
+    public void usaPista(Aereo aereo){
+
+        this.semaforoPiste.p();
+
+        System.out.println("\nla pista è in uso dal veicolo: "+aereo.getID());
+
+        //l'aereo usa la pista e poi conlude
+
+        System.out.println("\nla pista non è più in uso dal veicolo: "+aereo.getID());
+
+        this.semaforoPiste.v();
+    }
 }
