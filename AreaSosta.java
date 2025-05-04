662a40d2a6c4a9a7bb77a989d55f54a2510b110d
Filippo027 <filippomartellossi@gmail.com>
2025-05-04 22:10:40 +0200

    Add files via upload
    versione finale per area di sosta

--- AreaSosta.java
+++ AreaSosta.java
@@ -8,5 +8,13 @@ public class AreaSosta {
         this.semaforoSosta =new Semaforo(3);
     }
 
-    
+    public void entraInSosta(Aereo aereo) {
+        this.semaforoSosta.p();
+        System.out.println("\nnell'area di sosta sta entrando il veicolo: "+aereo.getiD());
+    }
+
+    public void esciDaSosta(Aereo aereo) {
+        System.out.println("\ndall'area di sosta sta uscendo il veicolo: "+aereo.getiD());
+        this.semaforoSosta.v();
+    }
 }
