59726047045d19cc47df30c439526fcf76b868f3
Filippo027 <filippomartellossi@gmail.com>
2025-05-04 21:44:30 +0200

    Add files via upload
    base per l'aerea di sosta

---
+++ AreaSosta.java
@@ -0,0 +1,12 @@
+public class AreaSosta {
+
+    //dichiarazione semaforo
+    private Semaforo semaforoSosta;
+
+    //costruttore
+    public AreaSosta(){
+        this.semaforoSosta =new Semaforo(3);
+    }
+
+    
+}
