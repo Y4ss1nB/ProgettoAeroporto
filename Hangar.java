51c8e47ca1a766a23e3da577c4c56cac257d2aa8
Filippo027 <filippomartellossi@gmail.com>
2025-05-04 21:51:43 +0200

    Add files via upload
    base per hangar

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+
+    //dichiarazione semaforo
+    private Semaforo semaforoHangar;
+
+    //costruttore
+    public Hangar(){
+        this.semaforoHangar = new Semaforo(5);
+    }
+
+    
+}
