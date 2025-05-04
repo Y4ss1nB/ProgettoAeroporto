6041bf1044a8c00685e8fa497ea7ed7f208576f9
Filippo027 <filippomartellossi@gmail.com>
2025-05-04 22:39:19 +0200

    Add files via upload
    versione finale hangar

--- Hangar.java
+++ Hangar.java
@@ -8,5 +8,13 @@ public class Hangar {
         this.semaforoHangar = new Semaforo(5);
     }
 
-    
+    public void entraInHangar(Aereo aereo) {
+        this.semaforoHangar.p();
+        System.out.println("\nsta entrando nel hangar l'aereo: "+ aereo.getiD());
+    }
+
+    public void esciDaHangar(Aereo aereo) {
+        System.out.println("\nsta uscendo dall hangar l'aereo: "+ aereo.getiD());
+        this.semaforoHangar.v();
+    }
 }
