57eeb5abf587f7779d59eb16a14b6f4b665df078
Filippo027 <filippomartellossi@gmail.com>
2025-05-05 21:54:36 +0200

    Add files via upload
    veicolo di rifornimento base

---
+++ VeicoloRifornimento.java
@@ -0,0 +1,12 @@
+public class VeicoloRifornimento {
+
+    //dichiarazione semaforo
+    private Semaforo semaforoVeicoloRifornimento;
+
+    //costruttore
+    public VeicoloRifornimento(){
+        this.semaforoVeicoloRifornimento = new Semaforo(1);
+    }
+
+   
+}
