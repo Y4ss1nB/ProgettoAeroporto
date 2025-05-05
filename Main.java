21d455685fa12051007934aec2decb1d742df13b
Filippo027 <filippomartellossi@gmail.com>
2025-05-05 23:11:33 +0200

    Add files via upload
    Main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Aereoporto aeroporto = new Aereoporto();
+
+        aeroporto.creaAerei();
+        aeroporto.avviaAerei(); 
+    }
+}
