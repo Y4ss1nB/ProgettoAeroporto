14aa6e434a424524b9d575f54205520a2ebff892
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-05 22:49:36 +0200

    Update Aereoporto.java
    scritta funzione per creare gli aerei

--- Aereoporto.java
+++ Aereoporto.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Aereoporto {
     //attributi
@@ -8,8 +9,9 @@ public class Aereoporto {
     private VeicoloRifornimento veicoloRifornimento;
     private VeicoloBagagli veicoloBagagli;
     private VeicoloPersone veicoloPersone;
-    
     private ArrayList<Aereo> insiemeAerei;
+
+    Random random = new Random();
 
     //costruttore
     public Aereoporto(){
@@ -22,5 +24,21 @@ public class Aereoporto {
         this.insiemeAerei = new ArrayList<Aereo>();
     }
 
+    public void creaAerei(){
+        int numAerei = 5;
+        for (int i = 0; i < numAerei; i++) {
+            String ID = "aereo" + i;
+            int codiceVolo = random.nextInt(2500);
+            String impresaCostrutrice = "Ryanair";
+            int pesoMax = random.nextInt(1000);
+            int passegeriMax = random.nextInt(500);
+            int numPersoneDaCaricare = random.nextInt(550);
+            int kgDaCaricare = random.nextInt(1100);
+            double maxLitriBenzia = random.nextDouble(100, 200);
+            boolean isSoloAndata = i % 2 == 0;
+            Aereo aereo = new Aereo(ID, isSoloAndata, codiceVolo, impresaCostrutrice, pesoMax, kgDaCaricare, numPersoneDaCaricare, passegeriMax, maxLitriBenzia, gestorePiste, areaSosta, hangar, veicoloRifornimento, veicoloBagagli, veicoloPersone);
+            insiemeAerei.add(aereo);
+        }
+    }
 
 }
