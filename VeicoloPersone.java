4fca6b8ce935adac85e066166a8bdb0dab7a5073
Filippo027 <filippomartellossi@gmail.com>
2025-05-05 17:15:32 +0200

    Add files via upload
    aggiunta funzione carica passegeri

--- VeicoloPersone.java
+++ VeicoloPersone.java
@@ -9,5 +9,18 @@ public class VeicoloPersone {
     }
 
 
+    public void caricaPasseggeri(Aereo aereo) {
+        semaforoVeicoloPersone.p();
+
+       if (aereo.getNumPersoneDaCaricare() > aereo.getNumPersoneMassime()) {
+            System.out.println("\n non ci sono posti per tutti alcune persone non saliranno; sono salite "+aereo.getNumPersoneMassime()+" sul veicolo "+aereo.getiD());
+            aereo.setNumPersoneEffetiveSuAereo(aereo.getNumPersoneMassime());
+       }else{
+            System.out.println("\nsono salitti tutti i passegeri, ovvero, "+aereo.getNumPersoneDaCaricare()+" sul veicolo "+aereo.getiD());
+       }
+
+        semaforoVeicoloPersone.v();
+    }
+
    
 }
