57f9d2079166b3b9e3155f278fcd1b0f27ba5726
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-05-05 22:05:25 +0200

    Update Aereoporto.java
    aggiunti attributi classe aereoporto

--- Aereoporto.java
+++ Aereoporto.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Aereoporto {
+    //attributi
+    private GestorePiste gestorePiste;
+    private AreaSosta areaSosta;
+    private Hangar hangar;
+    private VeicoloRifornimento veicoloRifornimento;
+    private VeicoloBagagli veicoloBagagli;
+    private VeicoloPersone veicoloPersone;
     
+    private ArrayList<Aereo> insiemeAerei;
 }
