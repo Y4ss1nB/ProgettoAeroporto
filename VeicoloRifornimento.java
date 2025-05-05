c577de0f9117bb707799f0072dc8139953333b82
Filippo027 <filippomartellossi@gmail.com>
2025-05-05 21:59:58 +0200

    Add files via upload
    veicolo rifornimento completo

--- VeicoloRifornimento.java
+++ VeicoloRifornimento.java
@@ -8,5 +8,12 @@ public class VeicoloRifornimento {
         this.semaforoVeicoloRifornimento = new Semaforo(1);
     }
 
-   
+    public void rifornisci(Aereo aereo) {
+        this.semaforoVeicoloRifornimento.p();
+
+        aereo.setLivelloBenzina(aereo.getMaxLitriSerbatoioBenzina()); //l'aereo viene rifornito
+        System.out.println("\nRifornito al massimo il veicolo: " + aereo.getiD());
+
+        this.semaforoVeicoloRifornimento.v();
+    }
 }
